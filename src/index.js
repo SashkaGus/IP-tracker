52847bdcf46f10303e495a8b29a4eb50b8168751
SashkaGus <a.a.gusev9866@gmail.com>
2022-11-28 16:46:21 +0300

    v.0.1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import {validateIp} from './helpers'
-import L from 'leaflet'
+import {addTileLayer, validateIp} from './helpers'
+import L, { marker } from 'leaflet'
 import 'leaflet/dist/leaflet.css';
+import icon from '../images/icon-location.svg';
 
 
 const mapArea = document.querySelector('.map')
@@ -11,23 +12,28 @@ const locationInfo = document.querySelector('#location')
 const timezoneInfo = document.querySelector('#timezone')
 const ispInfo = document.querySelector('#isp')
 
+const markerIcon = L.icon({
+    iconUrl: icon,
+    iconSize: [30, 40]
+    // iconAnchor: [22, 94]
+})
 
 const map = L.map(mapArea, {
     center:[51.5, 0],
-    zoom: 10
+    zoom: 10,
+    zoomControl: false
     
 });
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+
+addTileLayer(map)
+L.marker([51.5, 0], {icon: markerIcon}).addTo(map)
 
 btn.addEventListener('click', getData)
 ipInput.addEventListener('keydown', handleKey)
 
 function getData(){
     if (validateIp(ipInput.value)){  
-        fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_hdeIKxBMpQwxwhNJtqhaBPQkQH9TN&ipAddress=${ipInput.value}`)
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_hdeIKxBMpQwxwhNJtqhaBPQkQH9TN&ipAddress=${ipInput.value}`)
     .then(response => response.json())
     .then(setInfo)
     }
@@ -41,9 +47,12 @@ function handleKey(e){
 }
 
 function setInfo(mapData){
-    console.log(mapData)
+    const {lat, lng, country, region, timezone} = mapData.location;
+    debugger
     ipInfo.innerText = mapData.ip;
     locationInfo.innerText = `${mapData.location.country} ${mapData.location.region}`
     timezoneInfo.innerText = mapData.location.timezone
     ispInfo.innerText = mapData.isp
+    map.setView([lat, lng]);
+    L.marker([lat, lng], {icon: markerIcon}).addTo(map);
 }
